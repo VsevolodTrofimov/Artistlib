4bfdea9715ae9e13082926acf79ac3ca0c1a8cfe
Vsevolod Trofimov <seva.trofimov2@gmail.com>
2017-04-30 16:28:53 +0300

    Auth screen improved a bit

--- App/frontend/UI/pages/auth/authScreen.js
+++ App/frontend/UI/pages/auth/authScreen.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import GoogleLogin from 'react-google-login'
 
+import './auth.css'
+
 export default class AuthScreen extends React.Component {
   render() {
     return (
-      <div className='column items-center'>
+      <div className='auth column items-center'>
         <GoogleLogin
           clientId = { this.props.clientId }
           onSuccess = { this.props.onSuccess }
